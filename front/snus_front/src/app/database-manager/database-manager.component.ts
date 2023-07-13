eda245e88f8f6e3b34da374851d22a9ea14339aa
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2023-07-13 19:51:46 +0200

    created table design in html for output tags

--- front/snus_front/src/app/database-manager/database-manager.component.ts
+++ front/snus_front/src/app/database-manager/database-manager.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-database-manager',
   templateUrl: './database-manager.component.html',
   styleUrls: ['./database-manager.component.css']
 })
-export class DatabaseManagerComponent {
+export class DatabaseManagerComponent implements OnInit {
+ 
+  outputTags: TableOutputTag[] = [];
+  displayedColumns = ['name', 'type', 'description', 'value', 'actions'];
 
+  constructor(){}
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    let tag : TableOutputTag = {
+      id: "1",
+      description: "2",
+      value: "3",
+      unit: "4",
+      type: "5"
+    }
+    this.outputTags.push(tag);
+  }
+
+  handleButtonClick(tag: any){
+    
+  }
+
+
+}
+
+
+export interface TableOutputTag {
+  id: string,
+  description: string,
+  value: string,
+  unit: string,
+  type: string
 }
