8e5b031fbe85d79cad2e69a827d8287565172368
tince250 <t.s.mihajlovic25@gmail.com>
2023-07-13 20:26:16 +0200

    implemented console RTU simulation for updating IOAdress values

---
+++ RTU_Client/RTU_Client/IOEntryDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTU_Client
+{
+    public class IOEntryDTO
+    {
+        public string IOAddress { get; set; }
+        public double Value { get; set; }
+
+        public IOEntryDTO(IOEntry entry)
+        {
+            IOAddress = entry.IOAddress;
+            Value = entry.Value;
+        }
+    }
+}
---
+++ back/snus_back/snus_back/Services/IOEntryService.cs
@@ -0,0 +1,22 @@
+﻿using snus_back.DTOs;
+using snus_back.Models;
+using snus_back.Repositories;
+using snus_back.Services.ServiceInterfaces;
+
+namespace snus_back.Services
+{
+    public class IOEntryService : IIOEntryService
+    {
+        private IOEntryRepository allIOEntries;
+
+        public IOEntryService(IOEntryRepository entryRepository)
+        {
+            this.allIOEntries = entryRepository;
+        }
+
+        public void BatchUpdate(List<IOEntryDTO> dtos)
+        {
+            this.allIOEntries.BatchUpdate(dtos);
+        }
+    }
+}
